ff8de3aa456f9feea10c14476b14120f0ba55590
saulyzas <saulyzas38@gmail.com>
2025-01-25 21:49:24 +0200

    Update dorf1.php

--- dorf1.php
+++ dorf1.php
@@ -16,24 +16,24 @@ use App\Utils\AccessLogger;
 include_once("GameEngine/Village.php");
 AccessLogger::logRequest();
 
-if(isset($_GET['ok'])){
-	$database->updateUserField($session->uid,'ok', 0, 1);
+if (isset($_GET['ok'])) {
+	$database->updateUserField($session->uid, 'ok', 0, 1);
 	$_SESSION['ok'] = '0';
 }
 
-if(isset($_GET['newdid'])) {
-    $_SESSION['wid'] = $_GET['newdid'];
-    $database->query("UPDATE ".TB_PREFIX."users SET village_select=".$database->escape((int) $_GET['newdid'])." WHERE id=".$session->uid);  
-	header("Location: ".$_SERVER['PHP_SELF']);
+if (isset($_GET['newdid'])) {
+	$_SESSION['wid'] = $_GET['newdid'];
+	$database->query("UPDATE " . TB_PREFIX . "users SET village_select=" . $database->escape((int) $_GET['newdid']) . " WHERE id=" . $session->uid);
+	header("Location: " . $_SERVER['PHP_SELF']);
 	exit;
-} 
-else $building->procBuild($_GET);
+} else $building->procBuild($_GET);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
+
 <head>
 	<title><?php echo SERVER_NAME . ' - Village overview &raquo; ' . $village->vname; ?></title>
-	<link rel="shortcut icon" href="favicon.ico"/>
+	<link rel="shortcut icon" href="favicon.ico" />
 	<meta http-equiv="cache-control" content="max-age=0" />
 	<meta http-equiv="pragma" content="no-cache" />
 	<meta http-equiv="expires" content="0" />
@@ -45,78 +45,111 @@ else $building->procBuild($_GET);
 	<link href="<?php echo GP_LOCATE; ?>lang/en/compact.css?f4b7j" rel="stylesheet" type="text/css" />
 	<link href="<?php echo GP_LOCATE; ?>lang/en/lang.css?e21d2" rel="stylesheet" type="text/css" />
 	<?php
-	if($session->gpack == null || GP_ENABLE == false) {
-	echo "
-	<link href='".GP_LOCATE."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".GP_LOCATE."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
+	if ($session->gpack == null || GP_ENABLE == false) {
+		echo "
+	<link href='" . GP_LOCATE . "travian.css?e21d2' rel='stylesheet' type='text/css' />
+	<link href='" . GP_LOCATE . "lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
 	} else {
-	echo "
-	<link href='".$session->gpack."travian.css?e21d2' rel='stylesheet' type='text/css' />
-	<link href='".$session->gpack."lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
+		echo "
+	<link href='" . $session->gpack . "travian.css?e21d2' rel='stylesheet' type='text/css' />
+	<link href='" . $session->gpack . "lang/en/lang.css?e21d2' rel='stylesheet' type='text/css' />";
 	}
 	?>
 	<script type="text/javascript">
-	window.addEvent('domready', start);
+		window.addEvent('domready', start);
 	</script>
 </head>
 
 
 <body class="v35 ie ie8">
-<div class="wrapper">
-<img style="filter:chroma();" src="img/x.gif" id="msfilter" alt="" />
-<div id="dynamic_header">
-	</div>
-<?php include("Templates/header.tpl"); ?>
-<div id="mid">
-<?php include("Templates/menu.tpl"); ?>
-<div id="content"  class="village1">
-<h1><?php echo $village->vname; if($village->loyalty!='100'){ if($village->loyalty>'33'){ $color="gr"; }else{ $color="re"; } ?><div id="loyality" class="<?php echo $color; ?>"><?php echo LOYALTY; ?> <?php echo floor($village->loyalty); ?>%</div><?php } ?></h1>
-<div id="cap" align="left"><?php if($village->capital!='0') { echo "<font color=gray>(Capital)</font>"; } ?></div>
-<?php include("Templates/field.tpl");
-$timer = 1;
-?>
-<div id="map_details">
-<br /><br />
-<?php
-include("Templates/movement.tpl");
-include("Templates/production.tpl");
-include("Templates/troops.tpl");
+	<div class="wrapper">
+		<img style="filter:chroma();" src="img/x.gif" id="msfilter" alt="" />
+		<div id="dynamic_header">
+		</div>
+		<?php include("Templates/header.tpl"); ?>
+		<div id="mid">
+			<?php include("Templates/menu.tpl");
+			?>
+			<div id="content" class="village1">
+				<h1>
+						
+					</a>
+					<?php
+					echo $village->vname;
+					if ($village->loyalty == '100') {
+						if ($village->loyalty > '33') {
+							$color = "gr";
+						} else {
+							$color = "re";
+						} ?><div id="loyality" class="<?php echo $color; ?>" style="margin-left:60px;"><?php echo LOYALTY; ?> <?php echo floor($village->loyalty); ?>%</div><?php }
 
-if($building->NewBuilding) include("Templates/Building.tpl");
-?>
-</div>
-<br /><br /><br /><br /><div id="side_info">
-<?php
-include("Templates/multivillage.tpl");
-include("Templates/quest.tpl");
-include("Templates/news.tpl");
-if(!NEW_FUNCTIONS_DISPLAY_LINKS) {
-	echo "<br><br><br><br>";
-	include("Templates/links.tpl");
-}
-?>
-</div>
-<div class="clear"></div>
-</div>
-<div class="footer-stopper"></div>
-<div class="clear"></div>
+?></h1>
+				<?php if ($village->capital != '0') {
+					?>
+												<div id="loyality" class="cap" style="margin-left:9px;margin-top: -10px;"><font size=2 color=grey>(<?php if($session->lang == 'tr'){ echo 'Merkez'; }else{ echo 'Capital'; } ?>)</font></div>
+											<?php }else{
+												?>
+<div id="loyality" class="cap" style="margin-left:9px;margin-top: -10px;"><font size=2 color=grey>(<?php if($session->lang == 'tr'){ echo 'Köy'; }else{ echo 'Village'; } ?>)</font></div>
+												<?php
+											} ?>
+				<?php include("Templates/field.tpl");
+				$timer = 1;
+				?>
 
-<?php
-include("Templates/footer.tpl");
-include("Templates/res.tpl");
-?>
-<div id="stime">
-<div id="ltime">
-<div id="ltimeWrap">
-<?php echo CALCULATED_IN;?> <b><?php
-echo round(($generator->pageLoadTimeEnd()-$start_timer)*1000);
-?></b> ms
 
-<br /><?php echo SEVER_TIME;?> <span id="tp1" class="b"><?php echo date('H:i:s'); ?></span>
-</div>
-	</div>
-</div>
+				<div id="map_details">
+					<br>
+					<br>
+					<?php
+					include("Templates/movement.tpl");
+					include("Templates/production.tpl");
+					include("Templates/troops.tpl");
 
-<div id="ce"></div>
+					if ($building->NewBuilding) {
+						include("Templates/Building.tpl");
+					}
+
+					?>
+
+
+				</div>
+				<br><br><br><br>
+				<div id="side_info">
+
+					<?php
+					include("Templates/multivillage.tpl");
+					include("Templates/quest.tpl");
+					include("Templates/news.tpl");
+					if (!NEW_FUNCTIONS_DISPLAY_LINKS) {
+						echo "<br><br><br><br>";
+						include("Templates/links.tpl");
+					}
+					?>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="footer-stopper"></div>
+			<div class="clear"></div>
+
+			<?php
+			include("Templates/footer.tpl");
+			include("Templates/res.tpl");
+
+			?>
+			<div id="stime">
+				<div id="ltime">
+					<div id="ltimeWrap">
+						<?php echo CALCULATED_IN; ?> <b><?php
+														echo round(($generator->pageLoadTimeEnd() - $start_timer) * 1000);
+														?></b> ms
+
+						<br /><?php echo SEVER_TIME; ?> <span id="tp1" class="b"><?php echo date('H:i:s'); ?></span>
+					</div>
+				</div>
+			</div>
+
+			<div id="ce"></div>
 </body>
+
 </html>
+
